f646c799e5a71372382180dac1c13c1872a83611
gevorkia <liligevorkian@gmail.com>
2022-06-05 17:10:28 -0700

    Add api/appointments endpoint for req. #2

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,8 +1,26 @@
 class Api::AppointmentsController < ApplicationController
   def index
     # TODO: return all values
+
+    appointments = []
+
+    # using .includes to avoid n+1 queries
+    @appointments = Appointment.includes(:patient).includes(:doctor)
+
+    @appointments.each do |a| 
+        appointments.push({
+          id: a.id,
+          patient: { name: a.patient.name },
+          doctor: { name: a.doctor.name, id: a.doctor.id },
+          created_at: a.created_at,
+          start_time: a.start_time,
+          duration_in_minutes: a.duration_in_minutes
+        })
+    end
+
+    render json: appointments
+
     # TODO: return filtered values
-    head :ok
   end
 
   def create
